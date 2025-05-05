9b9aa5191685cf76b39edb1638acfb739dbfcf8f
Mike Chen <mcyc@users.noreply.github.com>
2025-05-05 17:25:57 -0400

    Added Home.py as the entrypoint page

---
+++ app/Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+# Page config
+st.set_page_config(page_title="InsightAtlas", layout="wide") #page_icon="🌐",
+
+# Main content
+st.title("Welcome to InsightAtlas")
+st.markdown("""
+**InsightAtlas** is a flexible dashboard for exploring and sharing geographic insights through data — 
+making it easy to map, compare, and communicate spatial trends using your own datasets.
+
+---
+
+### Overview
+
+Designed for data scientists, analysts, and civic-minded teams, InsightAtlas helps turn data into clear, 
+actionable stories — especially those with a regional or community focus. Key features include:
+
+- Prioritizing outreach based on local engagement trends  
+- Mapping access to education programs or community services  
+- Surfacing neighborhood-level metrics and disparities  
+- Creating interactive visual narratives to support decisions or policy  
+- Delivering lightweight tools for field teams and non-technical users  
+""")
+
+#st.image("", width=150)
